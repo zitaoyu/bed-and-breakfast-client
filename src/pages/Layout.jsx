15e74283e9ff85d290cec6282c07e5bdcb91c09d
zitaoyu <zitaoyu.work@gmail.com>
2023-12-21 02:34:50 -0800

    update naming conflit

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/navbar";
 import Footer from "../components/Footer";
 
 const Layout = () => {
